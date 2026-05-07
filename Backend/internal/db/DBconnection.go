184d3551a7278b600184af4c2d07d59c06540773
Marco <marcoleobre@gmail.com>
2026-05-06 21:07:25 -0600

    2do.Connection to the db

---
+++ Backend/internal/db/DBconnection.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
+)
+
+var DB *sql.DB
+
+func ConnectDB() {
+	var err error
+	err = godotenv.Load("../../.env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	DB, err = sql.Open("pgx", connStr)
+
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("Error pinging the database: ", err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
+
+}
